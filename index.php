174e60e745009338a6db6c5c23b1bc2196459255
BOUANAYA <bouanayasoufiane@gmail.com>
2025-04-15 17:45:51 +0200

    Add index.php with personal information display

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$name = "soufiane"; 
+$age = 26;
+$height = 1.68;
+$work = "developer";
+$hobby = "programming";
+$country = "Morocco";
+$city = "skhirat";
+$male = true;
+$married = false;
+$skills = array("HTML", "CSS", "JavaScript", "PHP" , "vuejs");
+// Displaying the variables
+
+echo "<h1>My name is $name</h1>";
+echo "<p>My age is $age</p>";
+echo "<p>My height is $height</p>";
+echo "<p>My work is $work</p>";
+echo "<p>My hobby is $hobby</p>";
+echo "<p>My country is $country</p>";
+echo "<p>My city is $city</p>";
+
+
+
+
+
+?>
